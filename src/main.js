7a65d8c86009deea17ffdbab46626f76c6903447
KitsdaGear <kitsada.sal@hotmail.com>
2020-08-12 16:27:25 +0700

    Solve Error

--- src/main.js
+++ src/main.js
@@ -3,7 +3,6 @@
 import Vue from "vue";
 import App from "./App";
 import router from "./router";
-import Vue from "vue";
 import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
 
 Vue.config.productionTip = false;
