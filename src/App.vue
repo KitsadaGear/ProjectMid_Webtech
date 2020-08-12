c55b2683f20959951368434a71c3b00a07c7aaf2
KitsdaGear <kitsada.sal@hotmail.com>
2020-08-12 17:25:50 +0700

    Connect Firebase

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,34 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <router-view />
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
-}
+  name: "App",
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+#main-menu {
+  width: 100%;
+  position: absolute;
+  top: 0;
+  z-index: 100;
+  transition-duration: 0.2s;
+  -webkit-transition-timing-function: linear;
+  transition-timing-function: linear;
+  background-color: none;
+  padding: 0 40px;
 }
 </style>
