d18155713599864b9b4299a94775ff16c4c88409
Bambiboo <varisara.ti@ku.th>
2020-08-16 17:48:50 +0700

    Detail_Contact_Refer_Update

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -1,7 +1,26 @@
-<template> </template>
+<template>
+  <div>
+    <h1>ติดต่อเรา</h1>
+    <h2>ติดต่อฝ่ายบริจาคหรือสอบถามทั่วไป: 02-999-9999, 089-999-9999</h2>
+    <h2>ติดต่อฝ่ายกิจกรรม: 089-999-9999</h2>
+    <h2>ติดต่อฝ่ายพัฒนา: 089-999-9999</h2>
+    <h2>ติดต่อฝ่ายแฟรนไชน์: 089-999-9999</h2>
+    <h2>ติดต่อชาวแบ่งปันกันได้ที่อีเมล: BangPanKan@xxx.com</h2>
+  </div>
+</template>
 
 <script>
 export default {};
 </script>
 
-<style></style>
+<style>
+h1, .h1 {
+    font-size: 4.0em;
+}
+h1 {
+    line-height: 4.5;
+}
+h2, .h2 {
+    font-size: 1.5rem;
+}
+</style>
